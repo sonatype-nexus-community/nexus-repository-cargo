b70cef34611132c8a86ae6ac61d1e6a5724ddfe1
Rick Altherr <rick.altherr@imperva.com>
2018-07-02 12:17:20 -0700

    Minimal UI for hosted repositories

---
+++ src/main/java/org/sonatype/nexus/plugins/cargo/ui/UiPluginDescriptorImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019, Imperva, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License Version 1.0, which accompanies this
+ * distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Imperva, the Imperva logo, SecureSphere, Incapsula, CounterBreach,
+ * ThreatRadar, Camouflage, Attack Analytics, Prevoty and design are trademarks
+ * of Imperva, Inc. and its subsidiaries. All other brand or product names are
+ * trademarks or registered trademarks of their respective holders.
+ */
+
+package org.sonatype.nexus.plugins.cargo.ui;
+
+import javax.annotation.Priority;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.rapture.UiPluginDescriptorSupport;
+
+@Named
+@Singleton
+@Priority(Integer.MAX_VALUE - 200)
+public class UiPluginDescriptorImpl
+        extends UiPluginDescriptorSupport
+{
+    public UiPluginDescriptorImpl() {
+        super("nexus-repository-cargo");
+        setNamespace("NX.cargo");
+        setConfigClassName("NX.cargo.app.PluginConfig");
+    }
+}
---
+++ src/main/resources/static/rapture/NX/cargo/app/PluginConfig.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2019, Imperva, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License Version 1.0, which accompanies this
+ * distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Imperva, the Imperva logo, SecureSphere, Incapsula, CounterBreach,
+ * ThreatRadar, Camouflage, Attack Analytics, Prevoty and design are trademarks
+ * of Imperva, Inc. and its subsidiaries. All other brand or product names are
+ * trademarks or registered trademarks of their respective holders.
+ */
+
+ Ext.define('NX.cargo.app.PluginConfig', {
+    '@aggregate_priority': 100,
+
+    requires: [
+        'NX.cargo.app.PluginStrings',
+        'NX.cargo.util.CargoRepositoryUrls'
+    ]
+});
