2b7862b87e41537cbb208c84e45fa769086fa2c2
Rick Altherr <rick.altherr@imperva.com>
2018-11-08 14:54:46 -0800

    Introduce CrateCoordinate as a unique crate identifier
    Uniquely identifies a crate name+version.  Provides methods for getting
    information about that crate such as filenames.

---
+++ src/main/java/org/sonatype/nexus/plugins/cargo/CrateCoordinates.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019, Imperva, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License Version 1.0, which accompanies this
+ * distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Imperva, the Imperva logo, SecureSphere, Incapsula, CounterBreach,
+ * ThreatRadar, Camouflage, Attack Analytics, Prevoty and design are trademarks
+ * of Imperva, Inc. and its subsidiaries. All other brand or product names are
+ * trademarks or registered trademarks of their respective holders.
+ */
+
+package org.sonatype.nexus.plugins.cargo;
+
+import com.vdurmont.semver4j.Semver;
+
+/* CrateCoordinates are a unique identifier for a specific crate instance.
+ * Since cargo registries have a flat namespace, only the crate name and version
+ * are required.
+ */
+public class CrateCoordinates
+{
+    protected String name;
+
+    protected Semver version;
+
+    public CrateCoordinates(String name, Semver version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Semver getVersion() {
+        return this.version;
+    }
+
+    public String getFileBasename() {
+        return String.format("%s-%s", this.name, this.version.getValue());
+    }
+}
