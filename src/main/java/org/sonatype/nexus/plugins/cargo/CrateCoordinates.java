11dfdb54a15c87ac0ae1cca317e6511fba67c7c5
Rick Altherr <rick.altherr@imperva.com>
2019-01-11 12:04:18 -0800

    Method to get a CrateCoordinate's index entry path

--- src/main/java/org/sonatype/nexus/plugins/cargo/CrateCoordinates.java
+++ src/main/java/org/sonatype/nexus/plugins/cargo/CrateCoordinates.java
@@ -41,4 +41,17 @@ public class CrateCoordinates
     public String getFileBasename() {
         return String.format("%s-%s", this.name, this.version.getValue());
     }
+
+    public String getIndexEntryPath() {
+        if (name.length() == 1) {
+            return String.format("1/%s", this.name);
+        } else if (name.length() == 2) {
+            return String.format("2/%s", this.name);
+        } else if (name.length() == 3) {
+            return String.format("3/%c/%s", this.name.charAt(0), this.name);
+        } else {
+            return String.format("%s/%s/%s", this.name.substring(0, 2), this.name.substring(2, 4),
+                    this.name);
+        }
+    }
 }
